54a6c84c8d46423b61873c7771b8b4b0d76de52a
shan-ti <thoselovelywords@gmail.com>
2025-07-20 21:45:48 +0900

    downgrade tailwind css to v3

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <div className='bg-brand-500 h-dvh mx-auto text-center py-10'>
-        <h1 className="text-teal-50 text-6xl">Hello Boilerplate</h1>
+      <div className='bg-brand-50 h-dvh mx-auto text-center py-10'>
+        <h1 className="text-gray-950 font-serif text-6xl">Hello Healtea App</h1>
       </div>
     </>
   )
