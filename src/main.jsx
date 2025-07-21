86cb7d631c04d90dd47187a9471ec6c498d586dc
shan-ti <thoselovelywords@gmail.com>
2025-07-21 16:39:07 +0900

    cleaned up

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app.css'
-import App from './App.jsx'
-import './i18n.js' // Import i18n configuration
-import { CurrencyProvider } from './context/CurrencyContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./app.css";
+import App from "./App.jsx";
+import "./i18n.js"; // Import i18n configuration
+import { CurrencyProvider } from "./context/CurrencyContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CurrencyProvider>
-    <App />
+      <App />
     </CurrencyProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
