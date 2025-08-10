12d9e8a379df655fe5f249c9212d3b4dc4dd2803
shan-ti <thoselovelywords@gmail.com>
2025-08-10 11:19:54 +0900

    add dynamic export for the images

---
+++ src/assets/images/fillerImg/index.js
@@ -0,0 +1,48 @@
+import assamBold from './assamBold.png';
+import autumnSpice from './autumnSpice.png';
+import coolKukicha from './coolKukicha.png';
+import darjeeling from './darjeeling.png';
+import earlGrey from './earlGrey.png';
+import emeraldEssence from './emeraldEssence.png';
+import healteaHero from './healteaHero.png';
+import herbalHarmony from './herbalHarmony.png';
+import hojicha from './hojichaBliss.png';
+import hojichaDrift from './hojichaDrift.png';
+import jadeLeafSerenity from './jadeLeafSerenity.png';
+import lemonBrew from './lemonBrew.png';
+import lemongrassZen from './lemongrassZen.png';
+import nuttyComfort from './nuttyComfort.png';
+import orangeSunrise from './orangeSunrise.png';
+import plumLimited from './plumLimited.png';
+import senchaMorningBreeze from './senchaMorningBreeze.png';
+import shinchaSunrise from './shinchaSunrise.png';
+import summerCrash from './summerCrash.png';
+import tropicalCitrusHarmony from './tropicalCitrusHarmony.png';
+import yuzuCitrusGlow from './yuzuCitrusGlow.png';
+
+export {
+  assamBold,
+  autumnSpice,
+  coolKukicha,
+  darjeeling,
+  earlGrey,
+  emeraldEssence,
+  healteaHero,
+  herbalHarmony,
+  hojicha,
+  hojichaDrift,
+  jadeLeafSerenity,
+  lemonBrew,
+  lemongrassZen,
+  nuttyComfort,
+  orangeSunrise,
+  plumLimited,
+  senchaMorningBreeze,
+  shinchaSunrise,
+  summerCrash,
+  tropicalCitrusHarmony,
+  yuzuCitrusGlow    
+}
+
+
+
